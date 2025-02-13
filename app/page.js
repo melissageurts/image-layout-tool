0b99efccea794798ef09989a2ac21095fcda7d6e
melissageurts <melissalynnegeurts@gmail.com>
2025-02-13 13:50:12 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+'use client';
+import ImageLayoutTool from '../components/ImageLayoutTool';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      <ImageLayoutTool />
+    </main>
+  );
+}
